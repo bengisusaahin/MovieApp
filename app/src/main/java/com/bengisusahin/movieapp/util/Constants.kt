95e3615b50295206081f5063e422c690ab3fd919
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-09-14 16:56:20 +0300

    feat: Created dto classes

---
+++ app/src/main/java/com/bengisusahin/movieapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.bengisusahin.movieapp.util
+
+object Constants{
+    const val API_KEY = "c7794418"
+    const val BASE_URL = "http://www.omdbapi.com/"
+    const val IMDB_ID = "imdb_id"
+
+}
