420a237d3a9baddd2009ee87d44a5430c6e597f7
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-09-15 17:10:43 +0300

    feat: Set navigation

--- app/src/main/java/com/bengisusahin/movieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/bengisusahin/movieapp/presentation/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bengisusahin.movieapp.presentation.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +20,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Screen.MovieScreen.route){
+                        composable(route = Screen.MovieScreen.route){
+
+                        }
+                        composable(route = Screen.MovieDetailScreen.route){
+
+                        }
+                    }
+                }
             }
         }
     }
